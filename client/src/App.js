b26dea94ed8530c34becadbd23b671fc1a537555
allensarmiento <allensarmiento3@gmail.com>
2020-04-09 17:42:01 -0700

    Add message and input styling and handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,41 @@
 import React, { useState, useEffect } from 'react';
 
 import Phone from './components/phone/Phone';
+import styles from './App.module.scss';
 
 const App = () => {
-  const [messages, setMessages] = useState({
-    leftPhone: [
-      'Message goes here...',
-      'Message goes here...',
-      'Message goes here...',
-      'Message goes here...',
-      'Message goes here...'
-    ],
-    rightPhone: []
-  });
+  const [leftPhone, setLeftPhone] = useState([]);
+  const [rightPhone, setRightPhone] = useState([]);
+
+  const sendMessageClicked = (phoneSender, messageToSend) => {
+    if (phoneSender === 'leftPhone') {
+      setLeftPhone(curMsgs => [...curMsgs, { type: 'sent', message: messageToSend }]);
+      setRightPhone(curMsgs => [...curMsgs, { type: 'received', message: messageToSend }]);
+    } else {
+      setRightPhone(curMsgs => [...curMsgs, { type: 'sent', message: messageToSend }]);
+      setLeftPhone(curMsgs => [...curMsgs, { type: 'received', message: messageToSend }]);
+    }
+  };
 
   return (
-    <div>
-      <Phone messages={messages.leftPhone} />
+    <div className={styles.App}>
+      <h1 className={styles.Title}>Text Translate using Google Translate API</h1>
+      <div className={styles.PhoneGroup}>
+        <div className={styles.PhoneContainer}>
+          <Phone 
+            messages={leftPhone} 
+            phoneIdentifier='leftPhone'
+            sendMessageClicked={sendMessageClicked}
+          />
+        </div>
+        <div className={styles.PhoneContainer}>
+          <Phone 
+            messages={rightPhone} 
+            phoneIdentifier='rightPhone'
+            sendMessageClicked={sendMessageClicked}
+          />
+        </div>
+      </div>
     </div>
   )
 };
--- client/src/components/phone/Phone.js
+++ client/src/components/phone/Phone.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MdSend } from 'react-icons/md';
 
 import TextBubble from '../text-bubble/TextBubble';
 import styles from './Phone.module.scss';
 
 const Phone = props => {
+  const [message, setMessage] = useState('');
+
+  const sendButtonClicked = () => {
+    props.sendMessageClicked(props.phoneIdentifier, message);
+    setMessage('');
+  };
+
   return (
     <div className={styles.Phone}>
       <div className={styles.Screen}>
         <div className={styles.MessageContainer}>
           {props.messages.map((phoneMessage, messageIndex) => (
-            <TextBubble key={messageIndex}>
-              {phoneMessage}
-            </TextBubble> 
+            <div 
+              className={phoneMessage.type === 'received' ? 
+                styles.ReceivedTextBubbleContainer : 
+                styles.SentTextBubbleContainer}
+            >
+              <TextBubble 
+                key={messageIndex}
+                className={styles.TextBubble}
+              >
+                {phoneMessage.message}
+              </TextBubble> 
+            </div>
           ))}
         </div>
         <div className={styles.InputContainer}>
-          <input type="text" placeholder="Enter a message..."/>
+          <input 
+            type="text" 
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+            placeholder="Enter a message..."
+          />
+          <button 
+            className={styles.SendBtn}
+            onClick={sendButtonClicked}
+          >
+            <MdSend />
+          </button>
         </div>
       </div>
     </div>
